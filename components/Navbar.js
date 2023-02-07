59ffe8faf67712dfb4408811174aa4c8d38ea8e0
Yevhen <leshchenkoyevhen@gmail.com>
2023-02-07 14:12:03 -0500

    add react-responsive and responsive layout

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,17 +3,35 @@ import menu from "../public/menu.svg";
 import logo from "../public/logo.svg";
 import Link from "next/link";
 import styles from "../styles/Navbar.module.css";
+// import MediaQuery from "react-responsive";
+
+import dynamic from "next/dynamic";
+
+const MediaQuery = dynamic(() => import("react-responsive"), {
+  ssr: false,
+});
 
 const Navbar = () => {
+  const navLinks = ["home", "works", "about-me", "contacts"];
+
   return (
     <header>
       <div className={styles.navbarWrap}>
         <div className={styles.navbarLogoWrap}>
           <Image className={styles.navbarLogo} src={logo} alt="logo" />
-          <Link className={styles.navbarLink} href="/">
+          <Link className={styles.navbarName} href="/">
             Yevhen
           </Link>
         </div>
+        <MediaQuery minWidth={768}>
+          <ul className={styles.navbarLinks}>
+            {navLinks.map((link) => (
+              <li key={link}>
+                <p>{link}</p>
+              </li>
+            ))}
+          </ul>
+        </MediaQuery>
         <Image src={menu} alt="menu icon" />
       </div>
     </header>
