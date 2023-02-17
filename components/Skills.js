15b83b44d357cf383fa148cecd9fba4b508413ff
Yevhen <leshchenkoyevhen@gmail.com>
2023-02-17 16:56:11 -0500

    add libraries to Skills

--- components/Skills.js
+++ components/Skills.js
@@ -16,23 +16,35 @@ const Skills = () => {
         <ul className={styles.skillsList}>
           <li className={styles.skillsItem}>
             <h3 className={styles.skillsName}>Languages</h3>
-            <p className={styles.skillsStack}>Typescript, JavaScript</p>
-          </li>
-          <li className={styles.skillsItem}>
-            <h3 className={styles.skillsName}>Databases</h3>
-            <p className={styles.skillsStack}>Mongo</p>
-          </li>
-          <li className={styles.skillsItem}>
-            <h3 className={styles.skillsName}>Tools</h3>
-            <p className={styles.skillsStack}>Git, VSCode</p>
-          </li>
-          <li className={styles.skillsItem}>
-            <h3 className={styles.skillsName}>Other</h3>
-            <p className={styles.skillsStack}>HTML, CSS</p>
+            <p className={styles.skillsStack}>Typescript JavaScript</p>
           </li>
           <li className={styles.skillsItem}>
             <h3 className={styles.skillsName}>Frameworks</h3>
-            <p className={styles.skillsStack}>React</p>
+            <p className={styles.skillsStack}>React Next.js</p>
+          </li>
+          {/* <li className={styles.skillsItem}>
+            <h3 className={styles.skillsName}>Databases</h3>
+            <p className={styles.skillsStack}>Mongo</p>
+          </li> */}
+          <li className={styles.skillsItem}>
+            <h3 className={styles.skillsName}>Tools</h3>
+            <p className={styles.skillsStack}>Git VSCode</p>
+          </li>
+          <li className={styles.skillsItem}>
+            <h3 className={styles.skillsName}>Other</h3>
+            <p className={styles.skillsStack}>
+              HTML CSS SASS Rest AJAX Redux RTK Webpack NPM BEM
+            </p>
+          </li>
+          <li className={styles.skillsItem}>
+            <h3 className={styles.skillsName}>Libraries</h3>
+            <p className={styles.skillsStack}>
+              Material UI Bootstrap Emotion Styled-Components Axios Formik Yup
+            </p>
+          </li>
+          <li className={styles.skillsItem}>
+            <h3 className={styles.skillsName}>Methodology</h3>
+            <p className={styles.skillsStack}>Agile/Scrum/Trello</p>
           </li>
         </ul>
       </div>
