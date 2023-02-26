387bcc3398734fe4ac886a019bb0d16fd2007970
Yevhen <leshchenkoyevhen@gmail.com>
2023-02-26 11:10:54 -0500

    refactoring re export

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,15 @@
 import Navbar from "./Navbar.js";
-import Footer from "components/Footer/Footer";
+import Footer from "./Footer/Footer";
 import styles from "styles/Layout.module.css";
 import Home from "./Home";
 import ModalNav from "./ModalNav";
 import SocList from "./SocList";
-import Projects from "components/Projects/Projects";
+import Projects from "./Projects/Projects";
 import { useState } from "react";
 import dynamic from "next/dynamic";
-import Skills from "components/Skills";
-import About from "components/About";
-import Contacts from "components/Contacts/Contacts";
+import Skills from "./Skills";
+import About from "./About";
+import Contacts from "./Contacts/Contacts";
 
 const MediaQuery = dynamic(() => import("react-responsive"), {
   ssr: false,
