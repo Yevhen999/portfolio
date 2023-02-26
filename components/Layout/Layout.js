f53a66079b874deeafcd47325e0bb57197151dc2
Yevhen <leshchenkoyevhen@gmail.com>
2023-02-26 11:30:05 -0500

    refactoring folder structure

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,14 +1,14 @@
-import Navbar from "../Navbar.js";
+import Navbar from "../Navbar/Navbar";
 import Footer from "../Footer/Footer";
 import styles from "./Layout.module.css";
-import Home from "../Home";
-import ModalNav from "../ModalNav";
-import SocList from "../SocList";
+import Home from "../Home/Home";
+import ModalNav from "../ModalNav/ModalNav";
+import SocList from "../SocList/SocList";
 import Projects from "../Projects/Projects";
 import { useState } from "react";
 import dynamic from "next/dynamic";
-import Skills from "../Skills";
-import About from "../About";
+import Skills from "../Skills/Skills";
+import About from "../About/About";
 import Contacts from "../Contacts/Contacts";
 
 const MediaQuery = dynamic(() => import("react-responsive"), {
